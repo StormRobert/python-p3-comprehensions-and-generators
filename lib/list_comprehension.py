c23c4a7a86244e2a97ab21f2dbd284afae5b4b6b
StormRobert <stormarasa2@gmail.com>
2023-11-27 13:53:22 +0300

    added even numbers

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nums = []
+    for num in num_list:
+        if num % 2 == 0:
+            even_nums.append(num)
+    return even_nums if even_nums else []
 
 def make_exclamation(sentence_list):
     pass
